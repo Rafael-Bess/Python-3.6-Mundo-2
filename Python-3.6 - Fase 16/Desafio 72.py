8e8fa9b3a87b12769137dfc8ee55b1eb4eec4364
Rafael Bess <140470059+Rafael-Bess@users.noreply.github.com>
2023-09-21 19:53:34 -0300

    Update Desafio 72.py

--- Python-3.6 - Fase 16/Desafio 72.py
+++ Python-3.6 - Fase 16/Desafio 72.py
@@ -5,8 +5,8 @@ cont = ('Zero', 'Um', 'Dois', 'Três', 'Quatro',
 while True:
     num = int(input('Digite um número entre 0 e 20: '))
     if 0 <= num <= 20:
-        break
-
-    print('Tente novamente.', end='')
-print(f'Você digitou o número {cont[num]}')
-
+        print(f'Você digitou o número {cont[num]}')
+        continua = str(input('Você deseja continuar? (S/N)').upper())
+        if continua == 'N':
+            break
+    else: print('Tente novamente, ', end='')
